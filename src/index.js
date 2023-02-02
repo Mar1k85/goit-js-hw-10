01c1b6ca21442ab131fe3607d60f066ae6b3e0db
Sergei Markov <mar11k1985855@gmail.com>
2023-02-02 15:41:41 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,3 +7,5 @@ const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
 const countryList = document.querySelector('.country-list');
 const countryInfo = document.querySelector('.country-info');
+
+inputEl.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
