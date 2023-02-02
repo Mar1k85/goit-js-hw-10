658d4a7c4eaf3136f99e9794c7ab72d3cdfe1773
Sergei Markov <mar11k1985855@gmail.com>
2023-02-02 15:41:07 +0200

    start adds

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import countries from './fetchCountries';
+import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
+
+const inputEl = document.querySelector('#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
